6648b670f5527bc962e98474bbca8e597e5a46f5
Álan Crístoffer <acristoffers@gmail.com>
2016-06-06 15:35:51 +0200

    Self-confirm email test

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,14 +1,30 @@
 class StaticController < ApplicationController
   def index
-    redirect_to about_url :format => :html
+    redirect_to about_url format: :html
   end
-  
+
   def about
   end
-  
+
   def pic
   end
-  
+
   def countdown
+  end
+
+  def confirm
+    require 'net/https'
+
+    url = URI.parse('https://api.pushover.net/1/messages.json')
+    req = Net::HTTP::Post.new(url.path)
+    req.set_form_data(token: 'ao3kCaM5TYhgdz9ZivG2tqpoCUsS9c',
+                      user: 'udRwY5jxtFMhYXdEaQyP2kcDfjpxR8',
+                      message: "#{params[:email]} just checked...")
+    res = Net::HTTP.new(url.host, url.port)
+    res.use_ssl = true
+    res.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    res.start { |http| http.request(req) }
+
+    send_file 'public/img.jpg', type: 'image/jpg', disposition: 'inline'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,19 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static#index'
-  
-  scope "(:locale)" do
+
+  scope '(:locale)' do
     get 'pic' => 'static#pic'
     get 'about' => 'static#about'
     get 'countdown' => 'static#countdown'
     get 'll' => 'static#localized_links'
   end
-  
+
   get '/:locale' => 'static#index'
-  
+  get '/p/:email' => 'static#confirm'
+
   match '*path' => redirect('/'), via: :get unless Rails.env.development?
-  
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
