7f2623f66e298b7dde31e487e34480022420220a
Álan Crístoffer <acristoffers@gmail.com>
2016-06-06 16:13:58 +0200

    Changes to email read verification test

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -15,11 +15,16 @@ class StaticController < ApplicationController
   def confirm
     require 'net/https'
 
+    emails = {
+      '0' => 'Álan',
+      '1' => 'Heider'
+    }
+
     url = URI.parse('https://api.pushover.net/1/messages.json')
     req = Net::HTTP::Post.new(url.path)
     req.set_form_data(token: 'ao3kCaM5TYhgdz9ZivG2tqpoCUsS9c',
                       user: 'udRwY5jxtFMhYXdEaQyP2kcDfjpxR8',
-                      message: "#{params[:email]} just checked...")
+                      message: "#{emails[params[:email]]} just checked...")
     res = Net::HTTP.new(url.host, url.port)
     res.use_ssl = true
     res.verify_mode = OpenSSL::SSL::VERIFY_PEER
