4e9d0c91bc4900f72448ce4391cf7bd80ff91caf
Álan Crístoffer <acristoffers@gmail.com>
2019-11-18 12:59:45 -0300

    Add Marcio's paper (Prototype Modelling for Real Systems)

--- webapp/src/views/Publications.vue
+++ webapp/src/views/Publications.vue
@@ -1,6 +1,43 @@
 <template>
   <div id="publications">
     <div class="center">
+      <h1>2019</h1>
+      <div class="publication">
+        <h2>
+          <a
+            href="https://sic.ici.ro/wp-content/uploads/2018/10/Art.-2-Issue-3-2018-SIC.pdf"
+            target="_blank"
+          >
+            FILHO, MARCIO R. O.; E SOUSA, ÁLAN C.; COSTA, EMERSON S.; LEITE,
+            VALTER J. DE S. (2019). XL CILAMCE: Prototype Modelling for Real
+            Systems.
+          </a>
+        </h2>
+        <div class="abstract">
+          Models represent physical phenomena mathematically. Through it, it's
+          possible to obtain mathematical expressions that describe the
+          behaviour of the real system, allowing, for example, to simulate its
+          evolution in time as a response to some input stimulus without
+          actually operating the real system. They also allow the creation of
+          control laws that are mathematically correct and adequate to the
+          system, allowing the development of formally correct and performant
+          controllers. In this study, a system of coupled tanks, present in
+          CEFET Campus V's Signals and System Analysis laboratory, was modelled
+          in 6 different configurations. The system is composed of 4 water
+          tanks, two on the top shelf and two on the bottom, with valves that
+          allow the water to flow to its sibling or the one directly below it.
+          The valves allow the user to chose how the water will flow through
+          them. We developed a model for each configuration using physical laws
+          of water flow in cylindrical recipients and estimated the values of
+          the constants through tests. The models are represented using
+          state-space, as it allows to identify the dynamics of the system
+          internal energy, as well as the use of modern control techniques. Each
+          model was validated against the real system to measure its
+          correctness. The models obtained are available for students of the
+          laboratory, both undergraduate and postgraduate, to streamline the
+          study of system analysis and control theory.
+        </div>
+      </div>
       <h1>2018</h1>
       <div class="publication">
         <h2>
@@ -8,7 +45,7 @@
             href="https://sic.ici.ro/wp-content/uploads/2018/10/Art.-2-Issue-3-2018-SIC.pdf"
             target="_blank"
           >
-            SOUSA, ÁLAN C. E.; LEITE, VALTER J. S. ; RUBIO SCOLA, IGNACIO .
+            SOUSA, ÁLAN C. E.; LEITE, VALTER J. S. ; RUBIO SCOLA, IGNACIO.
             Affordable Control Platform with MPC Application. Studies in
             Informatics and Control, v. 27, p. 265-274, 2018
           </a>
@@ -51,7 +88,7 @@
           The authors conduct experiments to locally
           stabilize a nonlinear system subject to actuator sat-uration. For that
           they utilize a real system of coupled tankswith a nonlinearity and
-          sensors and actuators withindustrial characteristics. Takagi-Sugeno’s
+          sensors and actuators withindustrial characteristics. Takagi-Sugeno's
           fuzzy modeling is adopted to obtain a representation of this systemin
           discrete time. The studied controller is composed of a state feedback
           and a discrete integrator in time withfuzzy-type gains. However the
@@ -74,8 +111,8 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import _ from 'lodash';
+import { Component, Vue } from "vue-property-decorator";
+import _ from "lodash";
 
 @Component
 export default class Publications extends Vue {}
@@ -99,6 +136,7 @@ export default class Publications extends Vue {}
   }
 
   .publication {
+    padding-bottom: 1rem;
     h2 {
       font-size: 12pt;
     }
