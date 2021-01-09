83956ab4ae1d02638a6ce051fede4ce3949b7ce5
Álan Crístoffer <acristoffers@gmail.com>
2021-01-09 12:19:09 -0300

    Generates release with changes.

--- webapp/src/views/About.vue
+++ webapp/src/views/About.vue
@@ -48,7 +48,7 @@ export default class About extends Vue {
     'Twitter': 'https://twitter.com/acristoffers',
     'Linkedin': 'https://www.linkedin.com/in/acristoffers',
     'Lattes': 'http://lattes.cnpq.br/0846174549224433',
-    'Research Gate': 'https://www.researchgate.net/profile/Alan_E_Sousa'
+    'Research Gate': 'https://www.researchgate.net/profile/Alan_E_Sousa',
   };
 
   public get links() {
@@ -57,7 +57,7 @@ export default class About extends Vue {
       'Apostila de Python 3': '/Python3.pdf#view=FitH',
       'Apostila de MATLAB': '/MATLAB.pdf#view=FitH',
       [this.$t('resume') as string]: '/resume.pdf#view=FitH',
-      [this.$t('gpg_public_key') as string]: '/PublicKey.asc'
+      [this.$t('gpg_public_key') as string]: '/PublicKey.asc',
     };
   }
 
@@ -66,7 +66,7 @@ export default class About extends Vue {
       [this.$t('pt') as string]: this.$t('native'),
       [this.$t('en') as string]: 'C1',
       [this.$t('de') as string]: 'C1',
-      [this.$t('fr') as string]: 'B1'
+      [this.$t('fr') as string]: 'B1',
     };
   }
 
