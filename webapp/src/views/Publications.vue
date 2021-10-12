11a7646a12416b1ddb83f404ce8a76b7d7074399
Álan Crístoffer <acristoffers@gmail.com>
2021-10-12 11:08:11 +0200

    Adds smart-grid article (under review).

--- webapp/src/views/Publications.vue
+++ webapp/src/views/Publications.vue
@@ -5,6 +5,35 @@
       <div class="publication">
         <h2>
           <a href="#" targe="_blank">
+            E SOUSA, ÁLAN C.; MESSAI, NADHIR; MANAMANNI, NOUREDINNE (2021).
+            International Journal of Critical Infrastructure Protection:
+            Load-altering attack detection on smart grids using functional observers.
+            (Under review)
+          </a>
+        </h2>
+        <div class="abstract">
+          Smart grids are becoming more common due to their capacity to
+          accommodate secondary sources, like green energies from solar panels
+          or wind farms. However, the attack surface also grows with more
+          equipment in the network, making it necessary to secure appropriately.
+          With more sensors distributed on the network, it becomes easier for an
+          attacker to hack into one and send false information to the central to
+          destabilize the power generation and distribution. Load-altering
+          attacks do precisely that and have a destructive potential since the
+          generator reaction can cause network instability. Traditional
+          techniques, like those based on the Kalman filter, for example, may
+          pose numerical issues due to the large size and sparsity of the system
+          matrices, failing to provide good results or wasting computational
+          resources. We propose an LMI-based approach to design a bank of
+          residual generators for functional observers to detect such attacks.
+          This approach has the advantage of using a reduced order arbitrary
+          dynamic system, making it suitable for large-scale smart grids, and
+          the use of LMI, allowing the easy insertion of restrictions.
+        </div>
+      </div>
+      <div class="publication">
+        <h2>
+          <a href="#" targe="_blank">
             E SOUSA, ÁLAN C.; LEITE, VALTER J. DE S.; LUCIA, WALTER; SILVA, LUÍS F. P. (2021).
             Journal of Control, Automation and Electrical Systems:
             Command Governor strategy based on region of attraction.
